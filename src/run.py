f36fa24ed37f7f7a73af2312ef2e5572e46765ff
cjc-ds <carl.jason.chang@gmail.com>
2021-10-21 23:34:17 +1300

    removed viewMask() from main

--- src/run.py
+++ src/run.py
@@ -18,20 +18,24 @@ def main(args):
         rescaler=args.rescaler
     )
 
-    mm.viewMask()
+    mm.saveMask()
 
 if __name__=="__main__":
     np.random.seed(420)
     parser = argparse.ArgumentParser(description='MaskMaker')
-    parser.add_argument('--regions', type=int, default=15, help='Number of regions')
+    parser.add_argument('--regions', type=int, default=15,
+                         help='Number of regions')
     random_positions = [(np.random.randint(256), np.random.randint(256))
                         for i in range(20)]
-    parser.add_argument('--shifts', type=list, default=random_positions)
+    parser.add_argument('--shifts', type=list, default=random_positions,
+                        help='List of translations in (x,y) direction.')
     random_edges = [np.random.randint(3,8) for i in range(20)]
-    parser.add_argument('--edges', type=list, default=random_edges)
+    parser.add_argument('--edges', type=list, default=random_edges,
+                        help='List of edges in the polygon')
     parser.add_argument('--dimensions', type=tuple, default=(256,256))
     parser.add_argument('--seed', type=int, default=420)
     parser.add_argument('--rescaler', type=int, default=21)
+    parser.add_argument('--mask_directory', type=str, default='mask.png')
     
     args = parser.parse_args()
     main(args)
